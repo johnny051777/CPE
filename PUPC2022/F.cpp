acb9aff776352c627413323777367305ce43ef98
johnny051777 <153641571+johnny051777@users.noreply.github.com>
2025-06-20 18:00:23 +0800

    Create F.cpp

---
+++ PUPC2022/F.cpp
@@ -0,0 +1,39 @@
+//
+//
+//   題目以紙本為準，系統內題目若有跑版或亂碼，應參酌紙本題目。
+//
+//
+//使用Java的考生請注意，最外層的類別(class)需命名為 main 。
+//如果遇到超乎想像的狀況，請更改編譯器試看看!! 各編譯器特性不同!!
+//預設測資、隨機測資、固定測資是用來幫助除錯用的。批改時，只看暗中測資是否通過!!
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int N;
+	cin>>N;
+	
+	int count=0;
+	string inps;
+	
+	while(N--){
+		vector<int> a(500,0);
+		cin>>inps;
+		for(int i=0 ; i<inps.length() ; i++){
+			inps[i] = toupper(inps[i]);
+		}
+		for(int i=0 ; i<inps.length() ; i++){
+			if(a[inps[i]]==0){
+				count++;
+				a[inps[i]]=1;
+			}
+		}
+		
+		if(count%3==0)cout<<"INSTANT COFFEE!"<<endl;
+		if(count%3==2)cout<<"HANGING EAR!"<<endl;
+		if(count%3==1)cout<<"AMERICANO COFFEE!"<<endl;
+		count = 0;
+	}
+}
